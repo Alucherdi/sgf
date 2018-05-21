c855fc2d1b9f095a293f1c191f348a87541c7ea5
Kiritodev <kiritodev01@gmail.com>
2018-05-21 18:52:09 -0500

    Adding some changes to news global context

--- src/components/Context/Storage.jsx
+++ src/components/Context/Storage.jsx
@@ -1,68 +1,91 @@
 import React, { Component } from 'react'
+import { Dropbox } from "dropbox"
 
 const Context = React.createContext();
 
-class Provider extends React.Component{
+class Provider extends React.Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            news: []
-        }        
-        var token = 'oUhHgt2EauAAAAAAAAAAIL7ZT6g3MU5cUjwau-ZdbEnKVzPNM8-z8UU_G2NTYlgV';
-        var Dropbox = require('dropbox').Dropbox;
-        var dbx = new Dropbox({ accessToken: token });               
-        dbx.filesListFolder({path: '/Noticias'}).then(response =>{
-            response.entries.map( data =>{
-                dbx.filesGetMetadata({path: data.path_display}).then( metadata => {   
-                    dbx.usersGetAccount({account_id: metadata.sharing_info.modified_by}).then(response => {
-                        dbx.filesGetTemporaryLink({path: data.path_display}).then( json => {                                         
-                            fetch(json.link).then( response =>{
-                                return response.text();
-                            }).then( text => {   
-                                this.setState(prevState => ({
-                                    news: [...prevState.news, JSON.stringify({
-                                        content: text,
-                                        file_id: metadata.id,
-                                        date: metadata.client_modified,
-                                        owner_id: response.account_id,
-                                        owner_name: response.name.display_name
-                                    })]
-                                }))
-                            })                                               
-                        })   
-                    })                          
-                })                  
-                return data;          
-            });                
-        }).catch(function(error) {
-            console.log(error);
-        });    
-    }
+	constructor(props) {
+		super(props);
 
-    render(){    
-       return(
-            <Context.Provider value={{
-                state: this.state
-            }}>
-                {this.props.children}
-            </Context.Provider>
-       );
-    }
+		this.state = {
+			dropboxToken: "oUhHgt2EauAAAAAAAAAAIL7ZT6g3MU5cUjwau-ZdbEnKVzPNM8-z8UU_G2NTYlgV",
+			news: []
+		}
+
+		var dbx = new Dropbox({ accessToken: this.state.dropboxToken });
+
+		this.dropboxFileData(dbx)
+		.then(all => {
+			this.setState({
+				news: all
+			})
+		})
+	}
+
+	dropboxFileData = (dbx) => {
+		return new Promise((resolve, reject) => {		
+			dbx.filesListFolder({ path: "/Noticias" })
+			.then(response => {
+				return new Promise((resolve, reject) => {
+					resolve(response.entries)
+				})
+			}).then((entries) => {
+				let promises = []
+				for (let [index, data] of entries.entries()) {
+					promises.push(new Promise((resolve, reject) => {
+						dbx.filesGetMetadata({ path: data.path_display })
+						.then(metadata => {
+							dbx.usersGetAccount({ account_id: metadata.sharing_info.modified_by })
+							.then(account => {
+								dbx.filesGetTemporaryLink({ path: data.path_display })
+								.then(json => {
+									fetch(json.link)
+									.then(jsonRes => {
+										return jsonRes.text()
+									})
+									.then(text => {
+										resolve({
+											content:    text,
+											file_id:    metadata.id,
+											date:       metadata.client_modified,
+											owner_id:   account.account_id,
+											owner_name: account.name.display_name
+										})
+									})
+								})
+							})
+						})
+					}))
+				}
+				resolve(Promise.all(promises))
+			})
+		})
+	}
+
+	render() {
+		return (
+			<Context.Provider value={{
+				state: this.state
+			}}>
+				{this.props.children}
+			</Context.Provider>
+		);
+	}
 }
 
-class SProvider extends Component{
-    render(){
-        return(
-            <Provider>
-                <Context.Consumer>
-                    {(context) => (
-                        <div>{React.cloneElement(this.props.children, {state: context.state})}</div>
-                    )}
-                </Context.Consumer>
-            </Provider>
-        )
-    }
+class SProvider extends Component {
+	render() {
+		return (
+			<Provider>
+				<Context.Consumer>
+					{(context) => (
+						<div>{React.cloneElement(this.props.children, { state: context.state })}</div>
+					)}
+				</Context.Consumer>
+			</Provider>
+		)
+	}
 }
 
 
