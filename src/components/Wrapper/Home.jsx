4525e90b550ee274f931170c4f12662bf41b8674
Alucherdi <hjgo127942@gmail.com>
2018-05-22 00:21:23 -0500

    Adding blogspot module to home

--- src/components/Wrapper/Home.jsx
+++ src/components/Wrapper/Home.jsx
@@ -7,21 +7,61 @@ import background from "../RocketIntro/background.png"
 
 import "./Home.scss"
 
-
 class Home extends React.Component {
 	render() {
 		return (
-			<div className="home" style={{backgroundImage: `url(${background})`}}>
+			<div className="home">
 				<NavBar />
 				<div className="modules">
 					<div className="module">
 						<div className="blogspot">
+							{this.blogspot().map((blog) => {
+								return blog
+							})}
 						</div>
 					</div>
 					<div className="module"></div>
 				</div>
 			</div>
 		)
+	}
+
+	blogspot = () => {
+		var image = "http://media.esports.eldesmarque.com/wp-content/uploads/2017/12/Rengar_1.jpg"
+		var m = []
+		for (var i = 0; i < 20; i ++) {
+			m.push(
+				<div className="blogspot_entry shadow">
+					<img src={image} alt="" className="blogspot_image"/>
+					<h1>Título</h1>
+					<p>
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+						Texto del blog Texto del blog Texto del blog 
+					</p>
+				</div>
+			)
+		}
+
+		return m
 	}
 }
 
