08f429c9e1cc81f185b74d7ea63d7b2e8fe354d3
Alucherdi <hjgo127942@gmail.com>
2018-06-05 02:14:30 -0500

    Fixing home sizing problems and adding login modal

--- src/components/NavBar/LoginModal.jsx
+++ src/components/NavBar/LoginModal.jsx
@@ -1,10 +1,19 @@
 import React from "react"
+import "./LoginModal.scss"
 
 class LoginModal extends React.Component {
 	render() {
 		return (
 			<div className="loginModal">
-				
+				<div className="head">
+					<div>Login</div>
+					<div>Sign up</div>
+				</div>
+				<form action="">
+					<input type="text" placeholder="Usuario"/>
+					<input type="password" placeholder="Password"/>
+					<input type="submit" value="Ingresar"/>
+				</form>
 			</div>
 		)
 	}
