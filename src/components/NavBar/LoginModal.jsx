14f48831a90e0c47ee3d5fc50371de6adfeb0723
Alucherdi <hjgo127942@gmail.com>
2018-06-06 01:17:34 -0500

    Adding login modal logic

--- src/components/NavBar/LoginModal.jsx
+++ src/components/NavBar/LoginModal.jsx
@@ -1,7 +1,30 @@
 import React from "react"
 import "./LoginModal.scss"
+import properties from "../Properties"
 
 class LoginModal extends React.Component {
+	sendLogin = (e) => {
+		e.preventDefault()
+		var formData = new FormData(e.currentTarget)
+
+		var params = {
+			method: "POST",
+			headers: { "Content-type": "application/json" },
+			body: JSON.stringify({
+				username: formData.get("username"), 
+				password: formData.get("password")
+			})
+		}
+		
+		fetch(properties.services.login, params).then(r => r.json()).then(data => {
+			if (data.code == 200) {
+				document.cookie = `user=${data.user.username}`
+				document.location.href = "/"
+			}
+			console.log(data)
+		})
+	}
+	
 	render() {
 		return (
 			<div className="loginModal">
@@ -9,9 +32,9 @@ class LoginModal extends React.Component {
 					<div>Login</div>
 					<div>Sign up</div>
 				</div>
-				<form action="">
-					<input type="text" placeholder="Usuario"/>
-					<input type="password" placeholder="Password"/>
+				<form onSubmit={this.sendLogin}>
+					<input type="text" placeholder="Usuario" name="username"/>
+					<input type="password" placeholder="Password" name="password"/>
 					<input type="submit" value="Ingresar"/>
 				</form>
 			</div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,10 +7,33 @@ import { Link } from "react-router-dom"
 import "./NavBar.scss"
 
 class NavBar extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			isLoginModalRendering: false,
+			isCookieSet: document.cookie.includes("user")
+		}
+	}
+	
+	bringModal = (isRendering) => {
+		this.setState((prev) => {
+			return {
+				isLoginModalRendering: !prev.isLoginModalRendering
+			}
+		})
+	}
+
+	updateCookie = () => {
+		
+	}
+	
 	render() {
+		var loginModal = this.state.isLoginModalRendering ? (<LoginModal />) : (<span></span>)
+		var loginButton = this.state.isCookieSet ? (<div className="option">Hi {document.cookie.split("=")[1]}</div>) : (<div className="option" onClick={this.bringModal}>Ingresar</div>)
 		return (
 			<div className="navbar">
-				<LoginModal />
+				{loginModal}
 				<div className="navbar_leftOptions">
 					<Link to="/" className="option">Home</Link>
 					<Link to={`${process.env.PUBLIC_URL}/news`} className="option">News</Link>
@@ -18,7 +41,7 @@ class NavBar extends React.Component {
 				</div>
 
 				<div className="navbar_rightOptions">
-					<div className="option">Ingresar</div>
+					{loginButton}
 				</div>
 			</div>
 		)
