ce918beeec952a0ff3083f08e0d024e1a7fd68bc
Kiritodev <kiritodev01@gmail.com>
2018-06-23 14:26:23 -0500

    Changing navbar css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -30,17 +30,20 @@ class NavBar extends React.Component {
 	
 	render() {
 		var loginModal = this.state.isLoginModalRendering ? (<LoginModal />) : (<span></span>)
-		var loginButton = this.state.isCookieSet ? (<div className="option">Hi {document.cookie.split("=")[1]}</div>) : (<div className="option" onClick={this.bringModal}>Ingresar</div>)
+		var loginButton = this.state.isCookieSet ? (<div className="option">Hi {document.cookie.split("=")[1]}</div>) : (<div className="option">Ingresar</div>)
 		return (
 			<div className="navbar">
 				{loginModal}
 				<div className="navbar_leftOptions">
-					<Link to="/" className="option">Home</Link>
-					<Link to={`${process.env.PUBLIC_URL}/news`} className="option">News</Link>
-					<Link to={`${process.env.PUBLIC_URL}/axtel`} className="option">Axtel</Link>
+					<Link to="/" className="option">Home</Link>			
 				</div>
-
-				<div className="navbar_rightOptions">
+				<div className="navbar_leftOptions">
+					<Link to={`${process.env.PUBLIC_URL}/news`} className="option">News</Link>
+				</div>				
+				<div className="navbar_leftOptions">
+					<Link to={`${process.env.PUBLIC_URL}/axtel`} className="option">Axtel</Link>
+				</div>									
+				<div className="navbar_rightOptions" onClick={this.bringModal}>
 					{loginButton}
 				</div>
 			</div>
