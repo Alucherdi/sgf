9307e4f6b12853918237a4ee269942b51f497af7
TOSHIBA\Ivan <irosales@luckyintelligence.com>
2018-07-12 23:23:54 -0500

    Colocacion de las banderas acorde a la luna

--- src/components/Alliances/Alliances.jsx
+++ src/components/Alliances/Alliances.jsx
@@ -2,15 +2,10 @@ import React from "react"
 import "./Alliances.scss"
 
 import NavBar from "../NavBar/NavBar";
+import SG_Moon from "./Assets/SG_Moon.png";
+import SG_Flag from "./Assets/SG_Flag.png";
 
 class Alliances extends React.Component {
-	puto() {
-		var divTarget = document.getElementById("target")
-
-		console.log(
-			divTarget.innerHTML
-		)
-	}
 	
 	render() {
 		return (
@@ -18,8 +13,19 @@ class Alliances extends React.Component {
 			<div>
 				<NavBar />
 				{/* Desde aquí empieza su html */}
-				<div id="target">contenido</div>
-				<button onClick={this.puto}>Click me</button>
+				<section class="main">
+					<div class="moondiv">
+						<img class="moon" src={SG_Moon}/>
+						
+						<img class="flag_arena" src={SG_Flag}/>
+						<img class="flag_gamelta" src={SG_Flag}/>
+						<img class="flag_gameplanet" src={SG_Flag}/>
+						<img class="flag_riot" src={SG_Flag}/>
+						<img class="flag_rocket9" src={SG_Flag}/>
+						<img class="flag_xbox" src={SG_Flag}/>
+						
+					</div>		
+				</section>
 			</div>
 			</React.Fragment>
 		)
