88db6409ec2d2f3d201844bead60e2e315a058fa
Linkaacm <link.sairento.hiru.29@gmail.com>
2018-07-14 15:42:32 -0500

    Fixing responsive elements: moon and flags

--- src/components/Alliances/Alliances.jsx
+++ src/components/Alliances/Alliances.jsx
@@ -2,25 +2,33 @@ import React from "react"
 import "./Alliances.scss"
 
 import NavBar from "../NavBar/NavBar";
+import SG_Moon from "./Assets/SG_Moon.png";
+import SG_Flag from "./Assets/SG_Flag.png";
 
 class Alliances extends React.Component {
-	puto() {
-		var divTarget = document.getElementById("target")
-
-		console.log(
-			divTarget.innerHTML
-		)
-	}
 	
 	render() {
 		return (
 			<React.Fragment>
-			<div>
-				<NavBar />
-				{/* Desde aquí empieza su html */}
-				<div id="target">contenido</div>
-				<button onClick={this.puto}>Click me</button>
-			</div>
+			<section class="wrapper">
+				<section class="main">
+
+				<div class ="navBarDiv">
+						<NavBar />
+				</div>
+
+					<div class="divWrapper">
+						<img class="moon" src={SG_Moon} />
+						<img class="flag_arena" src={SG_Flag} />
+						<img class="flag_gamelta" src={SG_Flag} />
+						<img class="flag_gameplanet" src={SG_Flag} />
+						<img class="flag_riot" src={SG_Flag} />
+						<img class="flag_rocket9" src={SG_Flag} />
+						<img class="flag_xbox" src={SG_Flag} />
+					</div>
+
+				</section>
+			</section>
 			</React.Fragment>
 		)
 	}
