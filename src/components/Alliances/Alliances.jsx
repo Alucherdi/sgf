0a70e415c0424fee31318a073693bf921ef441b7
TOSHIBA\Ivan <irosales@luckyintelligence.com>
2018-07-16 17:17:16 -0500

    Fixing responsive flags

--- src/components/Alliances/Alliances.jsx
+++ src/components/Alliances/Alliances.jsx
@@ -4,17 +4,25 @@ import "./Alliances.scss"
 import NavBar from "../NavBar/NavBar";
 import SG_Moon from "./Assets/SG_Moon.png";
 import SG_Flag from "./Assets/SG_Flag.png";
+import LogoArena from "./Assets/logoarena.png";
+import LogoGamelta from "./Assets/logogamelta.png";
+import LogoGamePlanet from "./Assets/logogameplanet.png";
+import LogoRiot from "./Assets/logoriot.png";
+import LogoXbox from "./Assets/logoxbox.png";
+import LogoRocket9 from "./Assets/rocket9.png";
 
 class Alliances extends React.Component {
 	
 	render() {
 		return (
 			<React.Fragment>
-			<div>
+			<div class="nav">
 				<NavBar />
-				{/* Desde aquí empieza su html */}
-				<section class="main">
-					<div class="moondiv">
+			</div>
+				
+			<section class="main">
+
+				<section class="moonWrapper">
 						<img class="moon" src={SG_Moon}/>
 						
 						<img class="flag_arena" src={SG_Flag}/>
@@ -23,10 +31,19 @@ class Alliances extends React.Component {
 						<img class="flag_riot" src={SG_Flag}/>
 						<img class="flag_rocket9" src={SG_Flag}/>
 						<img class="flag_xbox" src={SG_Flag}/>
-						
-					</div>		
+
+						<img class="logo_arena" src={LogoArena}/>
+						<img class="logo_gamelta" src={LogoGamelta}/>
+						<img class="logo_gameplanet" src={LogoGamePlanet}/>
+						<img class="logo_riot" src={LogoRiot}/>
+						<img class="logo_rocket9" src={LogoRocket9}/>
+						<img class="logo_xbox" src={LogoXbox}/>
 				</section>
-			</div>
+
+				<aside class="infoAllied">
+
+				</aside>
+			</section>
 			</React.Fragment>
 		)
 	}
