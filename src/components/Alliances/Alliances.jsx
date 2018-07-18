778c5c65e8710e45b6c0015fcb84abdb8883a370
TOSHIBA\Ivan <irosales@luckyintelligence.com>
2018-07-17 21:27:03 -0500

    Changed text-boxes for allies, info and image

--- src/components/Alliances/Alliances.jsx
+++ src/components/Alliances/Alliances.jsx
@@ -11,37 +11,63 @@ import LogoRiot from "./Assets/logoriot.png";
 import LogoXbox from "./Assets/logoxbox.png";
 import LogoRocket9 from "./Assets/rocket9.png";
 import Cuadro from "./Assets/cuadro.png";
+import Arena from "./Assets/arena.png";
+import Gamelta from "./Assets/gamelta.png";
+import GamePlanet from "./Assets/gameplanet.png";
+import Riot from "./Assets/riot.png";
+import Xbox from "./Assets/xbox.png";
 
 class Alliances extends React.Component {
 
-	arenaInfo(num){
-			var text= document.getElementById("textinfo")
+	arenaInfo(){
+		var divInfo = document.getElementById("divinfo")
+			divInfo.style.display = "block";
+
+		var text = document.getElementById("textinfo")
 			text.innerHTML = "Accede a horas gratis en Arena.";
+
+		var logo = document.getElementById("logoAliado")
+			logo.src = Arena;
 	}
 
 	gameltaInfo(){
 		var text= document.getElementById("textinfo")
 			text.innerHTML = "Asiste a los grandes torneos de videojuegos.";
+
+		var logo = document.getElementById("logoAliado")
+			logo.src = Gamelta;
 	}
 
 	gameplanetInfo(){
 		var text= document.getElementById("textinfo")
 			text.innerHTML = "Consigue decuentos para comprar tus videojuegos favoritos.";
+
+		var logo = document.getElementById("logoAliado")
+			logo.src = GamePlanet;
 	}
 
 	riotInfo(){
 		var text= document.getElementById("textinfo")
 			text.innerHTML = "Obten riotpoints gratis y otras regalos.";
+			
+		var logo = document.getElementById("logoAliado")
+			logo.src = Riot;
 	}
 
 	rocket9Info(){
 		var text= document.getElementById("textinfo")
 			text.innerHTML = "Equipate con contenido gratuito y especial.";
+
+		var logo = document.getElementById("logoAliado")
+			logo.src = LogoRocket9;
 	}
 
 	xboxInfo(){
 		var text= document.getElementById("textinfo")
 			text.innerHTML = "Descuento en GOLD y muchas sorpresas más.";
+
+		var logo = document.getElementById("logoAliado")
+			logo.src = Xbox;
 	}
 
 	render() {
@@ -70,8 +96,10 @@ class Alliances extends React.Component {
 						<img className="logo_rocket9" src={LogoRocket9} onClick={this.rocket9Info}/>
 						<img className="logo_xbox" src={LogoXbox} onClick={this.xboxInfo}/>
 
-						<div className="container">
+						<div className="container" id="divinfo">
 							<img className="textbox" src={Cuadro}/>
+							<img className="logoallie" id="logoAliado" />
+
 							<div className="text" id="textinfo"></div>
 						</div>
 						
